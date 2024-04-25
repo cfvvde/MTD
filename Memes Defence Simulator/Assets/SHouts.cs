140e21bded3bf7d979e9e7e500723c38ccb5d23e
cfvvde <cfvvde@gmail.com>
2024-04-26 00:19:02 +0300

    armstrong(finale)

--- Memes Defence Simulator/Assets/SHouts.cs
+++ Memes Defence Simulator/Assets/SHouts.cs
@@ -11,39 +11,37 @@ public class SHouts : MonoBehaviour
     public AudioClip audio2;
     public AudioClip audio3;
     private int rng;
-    private int count = 0;
-    void FixedUpdate()
+    float currentTime = 0;
+    float timeDiff = 3;
+
+    private void FixedUpdate()
     {
-        if (count == 10)
+        if (currentTime + timeDiff <= Time.time)
         {
+            currentTime = Time.time;
             this.GetComponent<AudioSource>().enabled = true;
             rng = Random.Range(0, 3);
-
-
             if (rng == 0)
             {
                 this.GetComponent<AudioSource>().clip = audio0;
-
+                this.GetComponent<AudioSource>().Play();
             }
             if (rng == 1)
             {
                 this.GetComponent<AudioSource>().clip = audio1;
-
+                this.GetComponent<AudioSource>().Play();
             }
             if (rng == 2)
             {
                 this.GetComponent<AudioSource>().clip = audio2;
+                this.GetComponent<AudioSource>().Play();
             }
             if (rng == 3)
             {
                 this.GetComponent<AudioSource>().clip = audio3;
+                this.GetComponent<AudioSource>().Play();
             }
-            count = 0;
         }
-        else
-        {
-            this.GetComponent<AudioSource>().enabled = false;
-            count++;
-        }
+
     }
 }
