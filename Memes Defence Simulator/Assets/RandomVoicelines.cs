2700fd1d0d0d3bc694a23ae100940a6ed7cdb1fa
AtQm1x <107392590+AtQm1x@users.noreply.github.com>
2024-04-26 01:42:46 +0300

    refactored some code
    armstrong no longer play musik
    :(   :(   :(   :(   :(

---
+++ Memes Defence Simulator/Assets/RandomVoicelines.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SHouts : MonoBehaviour
+{
+    public AudioClip audio0;
+    public AudioClip audio1;
+    public AudioClip audio2;
+    public AudioClip audio3;
+    private int rng;
+    private float currentTime = 0;
+    public float timeDiff = 3;
+
+    private void FixedUpdate()
+    {
+        if (currentTime + timeDiff <= Time.time)
+        {
+            currentTime = Time.time;
+            this.GetComponent<AudioSource>().enabled = true;
+            rng = new System.Random().Next(4);
+            switch (rng)
+            {
+                case 0:
+                    GetComponent<AudioSource>().clip = audio0;
+                    break;
+
+                case 1:
+                    GetComponent<AudioSource>().clip = audio1;
+                    break;
+
+                case 2:
+                    GetComponent<AudioSource>().clip = audio2;
+                    break;
+
+                case 3:
+                    GetComponent<AudioSource>().clip = audio3;
+                    break;
+            }
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
